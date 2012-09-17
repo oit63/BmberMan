fa7caaac72de2fe2d7bccfccae5014d64b2620db
njcit <njcit@njcitmatoiMac-2.local>
2012-09-17 18:56:55 +0800

    Initial Commit

---
+++ BmberMan/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BmberMan
+//
+//  Created by  on 12-9-17.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
